aa5219dc24f0d0b0024b092b87bfed16cffbec87
Luis Janeiro <jarolu@gmail.com>
2019-02-01 00:03:00 +0100

    Primer commit

---
+++ test/com/carballeira/katas/romannumerals/test/TestRomanNumerals.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.carballeira.katas.romannumerals.test;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author admin
+ */
+public class TestRomanNumerals {
+    
+    public TestRomanNumerals() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
